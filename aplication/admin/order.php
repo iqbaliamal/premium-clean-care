1e19ca10aef61ed9a1672c92dbcf2d13de05fea5
dimasheruperdanaputra <dimasptr87@gmail.com>
2020-12-31 20:00:22 +0700

    order dan order status

--- aplication/admin/order.php
+++ aplication/admin/order.php
@@ -4,72 +4,78 @@ if (isset($_SESSION['id']) && isset($_SESSION['nama'])) {
   require_once "header.php";
   require_once "../../config/koneksi.php";
 ?>
-  <!-- Begin Page Content -->
-  <div class="container-fluid">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Order</h1>
+  <!-- Page Heading -->
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Order</h1>
 
-      <nav aria-label="breadcrumb shadow">
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active" aria-current="page">Order</li>
-        </ol>
-      </nav>
+    <nav aria-label="breadcrumb shadow">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Order</li>
+      </ol>
+    </nav>
+  </div>
+
+  <div class="card mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">Data Order</h6>
     </div>
+    <div class="card-body">
 
-    <div class="card mb-4">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Data Order</h6>
+      <!-- ALERT -->
+      <?php if (isset($_GET['sukses'])) { ?>
+      <div class="alert alert-success" role="alert">
+        <?php echo $_GET['sukses'] ?>
       </div>
-      <div class="card-body">
+      <?php } ?>
+      <?php if (isset($_GET['error'])) { ?>
+      <div class="alert alert-danger" role="alert">
+        <?php echo $_GET['error'] ?>
+      </div>
+      <?php } ?>
+      <!-- END OF ALERT -->
 
-        <!-- ALERT -->
-        <div class="alert alert-success" role="alert">
-          Get alert sukses
-        </div>
-        <div class="alert alert-danger" role="alert">
-          Get alert danger
-        </div>
-        <!-- END OF ALERT -->
-
-        <?php
-        $query = $koneksi->query("SELECT * FROM order ORDER BY id_order ASC");
+      <?php
+        $query = $koneksi->query("SELECT * FROM `order` INNER JOIN `layanan` ON order.id_layanan=layanan.id_layanan INNER JOIN order_status ON order.id_order_status=order_status.id_order_status ORDER BY id_order ASC");
         $no = 1;
         while ($data = mysqli_fetch_assoc($query)) {
         ?>
-        <div class="col mb-2">
-          <div class="card border-left-primary shadow">
-            <div class="card-body">
-              <div class="row d-flex">
-                <div class="text-s mr-auto font-weight-bold text-uppercase mb-2"><?= $data['nama_layanan']; ?></div>
-                <div class="text-s mr-3 text-uppercase"><?= $data['order_date']; ?></div>
-                <div class="text-s text-uppercase">
-                  <span class="badge badge-primary"><?= $data['order_status']; ?></span>
-                </div>
+      <div class="col mb-2">
+        <div class="card border-left-primary shadow">
+          <div class="card-body">
+            <div class="row d-flex">
+              <div class="text-s mr-auto font-weight-bold text-uppercase mb-2"><?= $data['nama_layanan']; ?></div>
+              <div class="text-s mr-3 text-uppercase"><?= $data['order_date']; ?></div>
+              <div class="text-s text-uppercase">
+                <span class="badge badge-primary"><?= $data['order_status']; ?></span>
               </div>
-              <div class="row d-flex">
-                <div class="text-s mr-auto text-uppercase mb-2"><?= $data['nama_pelanggan']; ?></div>
-                <div class="text-s mr-5 text-uppercase"><?= $data['harga']; ?></div>
-                <div class="text-s font-weight-bold text-uppercase">
-                  <a href="detail_order.php" class="badge badge-dark">detail</a>
-                </div>
+            </div>
+            <div class="row d-flex">
+              <div class="text-s mr-auto text-uppercase mb-2"><?= $data['nama_pelanggan']; ?></div>
+              <div class="text-s mr-5 text-uppercase"><?= $data['harga']; ?></div>
+              <div class="text-s font-weight-bold text-uppercase">
+                <a href="detail_order.php" class="badge badge-dark">detail</a>
               </div>
-              <div class="row d-flex">
-                <div class="text-s mr-auto font-italic mb-2"><?= $data['merk']; ?> | <?= $data['warna']; ?> | <?= $data['ukuran']; ?></div>
-                <div class="text-s font-weight-bold">
-                  <button class="deletebtn border-0 btn-transition btn-sm btn-outline-danger" type="button"> <i class="fa fa-trash"></i> </button>
-                </div>
+            </div>
+            <div class="row d-flex">
+              <div class="text-s mr-auto font-italic mb-2"><?= $data['merk']; ?> | <?= $data['warna']; ?> |
+                <?= $data['ukuran']; ?></div>
+              <div class="text-s font-weight-bold">
+                <button class="deletebtn border-0 btn-transition btn-sm btn-outline-danger" type="button"> <i
+                    class="fa fa-trash"></i> </button>
               </div>
             </div>
           </div>
         </div>
-        <?php
+      </div>
+      <?php
         }
               ?>
 
-        <!-- <div class="col mb-2">
+      <!-- <div class="col mb-2">
           <div class="card border-left-warning shadow">
             <div class="card-body">
               <div class="row d-flex">
@@ -154,43 +160,51 @@ if (isset($_SESSION['id']) && isset($_SESSION['nama'])) {
     </div> -->
 
 
-    <!-- DELETE DATA -->
-    <div class="modal fade" id="deleteData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
+      <!-- DELETE DATA -->
+      <div class="modal fade" id="deleteData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
 
-          <form action="" method="POST">
-            <div class="modal-body">
-              <input type="hidden" name="delete_id" id="delete_id">
-              <h5> Apakah anda yakin akan menghapus data?</h5>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal"> Batal </button>
-              <button type="submit" name="deleteOrderStatus" class="btn btn-danger"> Hapus </button>
-            </div>
-          </form>
+            <form action="" method="POST">
+              <div class="modal-body">
+                <input type="hidden" name="delete_id" id="delete_id">
+                <h5> Apakah anda yakin akan menghapus data?</h5>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal"> Batal </button>
+                <button type="submit" name="deleteOrderStatus" class="btn btn-danger"> Hapus </button>
+              </div>
+            </form>
 
+          </div>
         </div>
       </div>
-    </div>
-    <!-- ======================================================================================== -->
+      <!-- ======================================================================================== -->
 
-  </div>
-  <!-- /.container-fluid -->
-  <?php
+    </div>
+    <!-- /.container-fluid -->
+    <?php
   require_once "footer.php";
   ?>
 
-  <!-- SCRIPT HAPUS DATA -->
-  <script>
-    $(document).ready(function() {
-      $('.deletebtn').on('click', function() {
-        $('#deleteData').modal('show');
-        // script disini
+    <!-- SCRIPT HAPUS DATA -->
+    <script>
+      $(document).ready(function () {
+        $('.deletebtn').on('click', function () {
+          $('#deleteData').modal('show');
+          $tr = $(this).closest('tr');
+        var data = $tr.children("td").map(function () {
+          return $(this).text();
+        }).get();
+
+        console.log(data);
+
+        $('#delete_id').val(data[1]);
+        });
       });
-    });
-  </script>
-<?php
+    </script>
+    <?php
 } else {
   header("Location: login.php");
   exit();
--- aplication/admin/order_status.php
+++ aplication/admin/order_status.php
@@ -4,193 +4,224 @@ if (isset($_SESSION['id']) && isset($_SESSION['nama'])) {
   require_once "header.php";
   require_once "../../config/koneksi.php";
 ?>
-  <!-- Begin Page Content -->
-  <div class="container-fluid">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-    <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Order Status</h1>
+  <!-- Page Heading -->
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Order Status</h1>
 
-      <nav aria-label="breadcrumb shadow">
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active" aria-current="page">Order Status</li>
-        </ol>
-      </nav>
+    <nav aria-label="breadcrumb shadow">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Order Status</li>
+      </ol>
+    </nav>
+  </div>
+
+  <!-- DataTales Example -->
+  <div class="card mb-4">
+    <div class="card-header py-3">
+      <h6 class="m-0 font-weight-bold text-primary">Order Status</h6>
     </div>
+    <div class="card-body">
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#tambahdata">
+        Tambah Data
+      </button>
 
-    <!-- DataTales Example -->
-    <div class="card mb-4">
-      <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">Order Status</h6>
+      <!-- ALERT -->
+      <?php if (isset($_GET['sukses'])) { ?>
+      <div class="alert alert-success" role="alert">
+        <?php echo $_GET['sukses'] ?>
       </div>
-      <div class="card-body">
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#tambahdata">
-          Tambah Data
-        </button>
+      <?php } ?>
+      <?php if (isset($_GET['error'])) { ?>
+      <div class="alert alert-danger" role="alert">
+        <?php echo $_GET['error'] ?>
+      </div>
+      <?php } ?>
+      <!-- END OF ALERT -->
 
-        <!-- ALERT -->
-        <div class="alert alert-success" role="alert">
-          Get alert sukses
-        </div>
-        <div class="alert alert-danger" role="alert">
-          Get alert danger
-        </div>
-        <!-- END OF ALERT -->
 
-        <?php
+      <?php
         $query = $koneksi->query("SELECT * FROM order_status ORDER BY id_order_status ASC");
         $no = 1;
-        while ($data = mysqli_fetch_assoc($query)) {
+        
         ?>
-        <div class="table-responsive">
-          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-            <thead>
-              <tr>
-                <th><?= $no++; ?></th>
-                <th hidden><?= $data['id_order_status']; ?></th>
-                <th><?= $data['order_status']; ?></th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tfoot>
+      <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th hidden>id</th>
+              <th>Status></th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th>No</th>
+              <th hidden>id</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </tfoot>
+          <tbody>
             <?php
-        }
-        ?>
-                  <button class="editbtn border-0 btn-transition btn btn-outline-warning" type="button"> <i class="fa fa-edit"></i> </button>
-                  <button class="deletebtn border-0 btn-transition btn btn-outline-danger" type="button"> <i class="fa fa-trash"></i> </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+            while ($data = mysqli_fetch_assoc($query)) {
+            ?>
+            <tr>
+              <td><?= $no++; ?></td>
+              <td><?= $data['id_order_status']; ?></td>
+              <td><?= $data['order_status']; ?></td>
+              <td>
+                <button class="editbtn border-0 btn-transition btn btn-outline-warning" type="button"> <i
+                    class="fa fa-edit"></i> </button>
+                <button class="deletebtn border-0 btn-transition btn btn-outline-danger" type="button"> <i
+                    class="fa fa-trash"></i> </button>
+              </td>
+            </tr>
+            <?php } ?>
+          </tbody>
+        </table>
       </div>
     </div>
-
-    <!-- Modal TAMBAH DATA -->
-    <div class="modal fade" id="tambahdata" tabindex="-1" role="dialog" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <form action="" method="POST">
-            <div class="modal-body">
-
-              <div class="form-group">
-                <label>Nama Status</label>
-                <input type="text" name="nama_status" class="form-control" placeholder="Nama Status" required>
-              </div>
-
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="submit" name="addOrderStatus" class="btn btn-primary">Simpan</button>
-            </div>
-          </form>
-
-        </div>
-      </div>
-    </div>
-    <!-- ======================================================================= -->
-
-    <!-- EDIT DATA -->
-    <div class="modal fade" id="editData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Edit Data</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <form action="" method="POST">
-            <div class="modal-body">
-
-              <input type="hidden" name="update_id" id="update_id">
-              <div class="form-group">
-                <label>Nama Status</label>
-                <input type="text" name="nama_status" id="orderStatus" class="form-control" placeholder="Nama Status" required>
-              </div>
-
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-              <button type="submit" name="editOrderStatus" class="btn btn-primary">Update</button>
-            </div>
-          </form>
-
-        </div>
-      </div>
-    </div>
-    <!-- =============================================================================================== -->
-
-    <!-- DELETE DATA -->
-    <div class="modal fade" id="deleteData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-
-          <form action="" method="POST">
-            <div class="modal-body">
-              <input type="hidden" name="delete_id" id="delete_id">
-              <h5> Apakah anda yakin akan menghapus data?</h5>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal"> Batal </button>
-              <button type="submit" name="deleteOrderStatus" class="btn btn-danger"> Hapus </button>
-            </div>
-          </form>
-
-        </div>
-      </div>
-    </div>
-    <!-- ======================================================================================== -->
-
-
   </div>
-  <!-- /.container-fluid -->
-  <?php
+
+  <!-- Modal TAMBAH DATA -->
+  <div class="modal fade" id="tambahdata" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="fungsi/fungsi_order_status.php" method="POST">
+          <div class="modal-body">
+
+            <div class="form-group">
+              <label>Nama Status</label>
+              <input type="text" name="nama_status" class="form-control" placeholder="Nama Status" required>
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" name="addOrderStatus" class="btn btn-primary">Simpan</button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+  <!-- ======================================================================= -->
+
+  <!-- EDIT DATA -->
+  <div class="modal fade" id="editData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Edit Data</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="fungsi/fungsi_order_status.php" method="POST">
+          <div class="modal-body">
+
+            <input type="hidden" name="update_id" id="update_id">
+            <div class="form-group">
+              <label>Nama Status</label>
+              <input type="text" name="nama_status" id="orderStatus" class="form-control" placeholder="Nama Status"
+                required>
+            </div>
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" name="editOrderStatus" class="btn btn-primary">Update</button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+  <!-- =============================================================================================== -->
+
+  <!-- DELETE DATA -->
+  <div class="modal fade" id="deleteData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+
+        <form action="fungsi/fungsi_order_status.php" method="POST">
+          <div class="modal-body">
+            <input type="hidden" name="delete_id" id="delete_id">
+            <h5> Apakah anda yakin akan menghapus data?</h5>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal"> Batal </button>
+            <button type="submit" name="deleteOrderStatus" class="btn btn-danger"> Hapus </button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </div>
+  <!-- ======================================================================================== -->
+
+
+</div>
+<!-- /.container-fluid -->
+<?php
   require_once "footer.php";
   ?>
 
-  <!-- SCRIPT HAPUS DATA -->
-  <script>
-    $(document).ready(function() {
-      $('#dataTable tbody').on('click', '.deletebtn', function() {
-        //alert("you activate the event");
-        $('.deletebtn').on('click', function() {
-          $('#deleteData').modal('show');
-          // script disini
-        });
+<!-- SCRIPT HAPUS DATA -->
+<script>
+  $(document).ready(function () {
+    $('#dataTable tbody').on('click', '.deletebtn', function () {
+      //alert("you activate the event");
+      $('.deletebtn').on('click', function () {
+        $('#deleteData').modal('show');
+        $tr = $(this).closest('tr');
+        var data = $tr.children("td").map(function () {
+          return $(this).text();
+        }).get();
+
+        console.log(data);
+
+        $('#delete_id').val(data[1]);
       });
     });
-  </script>
+  });
+</script>
 
-  <!-- SCRIPT EDIT DATA -->
-  <script>
-    $(document).ready(function() {
-      $('#dataTable tbody').on('click', '.editbtn', function() {
-        $('.editbtn').on('click', function() {
-          $('#editData').modal('show');
+<!-- SCRIPT EDIT DATA -->
+<script>
+  $(document).ready(function () {
+    $('#dataTable tbody').on('click', '.editbtn', function () {
+      $('.editbtn').on('click', function () {
+        $('#editData').modal('show');
 
-          $tr = $(this).closest('tr');
+        $tr = $(this).closest('tr');
 
-          var data = $tr.children("td").map(function() {
-            return $(this).text();
-          }).get();
+        var data = $tr.children("td").map(function () {
+          return $(this).text();
+        }).get();
 
-          console.log(data);
+        console.log(data);
 
-          $('#update_id').val(data[1]);
-          $('#orderStatus').val(data[2]);
-        });
+        $('#update_id').val(data[1]);
+        $('#orderStatus').val(data[2]);
       });
     });
-  </script>
+  });
+</script>
 <?php
 } else {
   header("Location: login.php");
